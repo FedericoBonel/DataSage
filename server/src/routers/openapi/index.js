7b5539ab6acea9b0271fe08a1e06ea760d42e7a4
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2024-03-28 17:05:08 +0900

    Added OpenAPI and swaggerui config and support

---
+++ server/src/routers/openapi/index.js
@@ -0,0 +1,68 @@
+import { Router } from "express";
+import swaggerJs from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import config from "../../config/index.js";
+import { messages } from "../../utils/constants/index.js";
+
+const router = Router();
+
+const openAPIConfig = {
+    definition: {
+        failOnErrors: true,
+        openapi: "3.1.0",
+        info: {
+            title: messages.openapi.TITLE,
+            version: messages.openapi.VERSION,
+            description: messages.openapi.DESC,
+            license: {
+                name: messages.openapi.license.NAME,
+                url: messages.openapi.license.URL,
+            },
+            contact: {
+                name: messages.openapi.contact.NAME,
+                url: messages.openapi.contact.URL,
+                email: messages.openapi.contact.EMAIL,
+            },
+        },
+        servers: [
+            {
+                url: config.server.urls.test,
+                description: messages.openapi.servers.TEST_DESC,
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+                cookieAuth: {
+                    type: "apiKey",
+                    in: "cookie",
+                    name: "jwt",
+                },
+            },
+        },
+        // TODO: Add security when implemented
+        security: [],
+    },
+    // Files containing API Specs
+    apis: ["./src/routers/*.js", "./src/dtos/*.js", "./src/utils/responsebodies/*.js"],
+};
+
+const openApiSpecs = swaggerJs(openAPIConfig);
+
+/** Handles requests that require OpenAPI specs in json format */
+router.route("/json").get((req, res) => res.send(openApiSpecs));
+
+/** Returns swagger ui page with OpenAPI specs */
+router.use(
+    swaggerUi.serve,
+    swaggerUi.setup(openApiSpecs, {
+        customCssUrl: config.swagger.theme,
+        swaggerOptions: { docExpansion: "none" },
+    })
+);
+
+export default router;
--- server/src/utils/constants/index.js
+++ server/src/utils/constants/index.js
@@ -1,10 +1,20 @@
 /** Imports and exports all constants in an encapsulated manner */
-import infoMessages from "./messages/info.js"
+import infoMessages from "./messages/info.js";
 import errorMessages from "./messages/errors.js";
+import openapiMessages from "./messages/openapi.js";
+import swaggeruiRoutes from "./routes/swaggerui.js";
 
+/** Object with all messages to be shown to the user */
 export const messages = Object.freeze({
     // Informational messages
     info: infoMessages,
     // General errors
     errors: errorMessages,
+    // OpenAPI messages
+    openapi: openapiMessages,
+});
+
+/** Object with all the routes used in the application without "/" (i.g. the route in the api "/entity" would be in routes.entity as "entity") */
+export const routes = Object.freeze({
+    swaggerUi: swaggeruiRoutes,
 });
