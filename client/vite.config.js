a82ef7d031cb42ab15655655744cb2ad940bb17f
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2024-04-22 11:31:40 +0900

    Added bundle visualizer plugin in vite.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), visualizer()],
     server: {
         port: 3000,
         open: true,
