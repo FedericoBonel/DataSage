d6f9bbb58121a4d8c1126ec569b8ef7920cd7123
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2024-05-02 11:49:05 +0900

    Added authorization in message generation and retrieval. Now users that dont have the required permissions to see documents can't see AI sources.

--- server/src/services/auth/auth.js
+++ server/src/services/auth/auth.js
@@ -1,6 +1,7 @@
 import colaboratorsRepository from "../../repositories/colaborators/colaborators.js";
 import { ForbiddenError, NotFoundError } from "../../utils/errors/index.js";
 import { messages } from "../../utils/constants/index.js";
+import verifyPermissions from "../../utils/permissions/verifyChatPermissions.js";
 
 /**
  * Authorizes a user to do some action in a chat
@@ -8,28 +9,24 @@ import { messages } from "../../utils/constants/index.js";
  * NOTE: If the user is the owner of the chat then no actions are checked. They are allowed full access.
  * @param {string} chatId Id of the chat where the request is being made
  * @param {string} userId Id of the user where that is making the request
- * @param {Array.<string>} allowedActions Array of the actions that are needed to commit the action
+ * @param {Array.<string>} requiredActions Array of the actions that are needed to commit the action
  */
-const authorizeCollaboratorToChat = async (chatId, userId, allowedActions) => {
+const authorizeCollaboratorToChat = async (chatId, userId, requiredActions) => {
     const colaboratorInstance = await colaboratorsRepository.getByChatAndUser(chatId, userId);
 
-    // Check if the user is the owner
-    if (colaboratorInstance?.chat.owner._id.toString() === userId) {
-        return;
-    }
-
-    // Check if the chat exists
+    // Check if the chat exists for the user
     if (!colaboratorInstance) {
         throw new NotFoundError(messages.errors.ROUTE_NOT_FOUND);
     }
 
-    // Verify required actions
-    const colabAllowedActions = {};
-    colaboratorInstance?.permissions.forEach((permission) => {
-        colabAllowedActions[permission.allowedAction] = true;
-    });
+    // Verify permissions and ownership
+    const hasAccess = verifyPermissions(
+        requiredActions,
+        colaboratorInstance.permissions,
+        colaboratorInstance.chat.owner._id.toString() === colaboratorInstance.user._id.toString()
+    );
 
-    if (!allowedActions.every((requiredAction) => colabAllowedActions[requiredAction])) {
+    if (!hasAccess) {
         throw new ForbiddenError(messages.errors.auth.FORBIDDEN);
     }
 };
